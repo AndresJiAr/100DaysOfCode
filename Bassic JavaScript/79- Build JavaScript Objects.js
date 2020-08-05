f5347c60a7c2662a2e6b93be7c25aa03d8fa8706
AndresJiAr <admin@AndresJiAr.dev>
2020-08-05 10:43:15 +0200

    Create 79- Build JavaScript Objects.js
    feat: Build JavaScript Objects

---
+++ Bassic JavaScript/79- Build JavaScript Objects.js
@@ -0,0 +1,10 @@
+var myDog = {
+    // Only change code below this line
+    "name": "Max",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["humans", "dogs"]
+    
+    // Only change code above this line
+    };
+    
