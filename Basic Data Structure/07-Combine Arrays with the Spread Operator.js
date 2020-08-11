9062c86fae966c10b4604c0efccf95eabefffc27
AndresJiAr <admin@AndresJiAr.dev>
2020-08-11 11:19:32 +0200

    Create 07-Combine Arrays with the Spread Operator.js
    feat: Combine Arrays with the Spread Operator

---
+++ Basic Data Structure/07-Combine Arrays with the Spread Operator.js
@@ -0,0 +1,8 @@
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());
+  
