3e04c658fa340af34054159f250b713587632214
AndresJiAr <admin@AndresJiAr.dev>
2020-08-13 11:18:42 +0200

    Update 10-Title Case a Sentence.js

--- Basic Algorithm Scripting/10-Title Case a Sentence.js
+++ Basic Algorithm Scripting/10-Title Case a Sentence.js
@@ -5,3 +5,23 @@ function titleCase(str) {
   
   console.log(titleCase("I'm a little tea pot"));
   
+/*(function titleCase(str) {
+  let result = "";
+  let words = str.split(" ")
+
+for (let i = 0; i < words.length; i+= 1) {
+      let word = words[i];
+
+    for (let j = 0; j < words.lenth; j += 1){
+          if (j === 0) {
+              result += word[j].toUpperCase();
+          } else {
+              result += word[j].toLowerCase();
+            }
+    }
+    if (words.length -1 !==i){
+        result += " ";
+    }
+}
+return result;
+  })*/
