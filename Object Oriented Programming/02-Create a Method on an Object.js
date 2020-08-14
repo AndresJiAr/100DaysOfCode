32b65223ed8e77a6d9bd200ce497791f10eaa85f
AndresJiAr <admin@AndresJiAr.dev>
2020-08-14 19:23:03 +0200

    Create 02-Create a Method on an Object.js
    feat: Create a Method on an Object

---
+++ Object Oriented Programming/02-Create a Method on an Object.js
@@ -0,0 +1,11 @@
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {
+      return "This dog has " + dog.numLegs + " legs.";
+    }
+  };
+  
+  
+  console.log(dog.sayLegs());
+  
