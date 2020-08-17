836800517cb282e8437478d8f1f649eb6d40a799
AndresJiAr <admin@AndresJiAr.dev>
2020-08-17 17:53:00 +0200

    Create 19-Reset an Inherited Constructor Property.js
    feat: Reset an Inherited Constructor Property

---
+++ Object Oriented Programming/19-Reset an Inherited Constructor Property.js
@@ -0,0 +1,16 @@
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+
+let beagle = new Dog();
+
