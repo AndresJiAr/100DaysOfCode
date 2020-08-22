8040373a1a4d36504fa4bbf9f0a0db8252fa4951
AndresJiAr <admin@AndresJiAr.dev>
2020-08-22 18:32:19 +0200

    Create 05-Map Dispatch to Props.js
    feat: Map Dispatch to Props

---
+++ React and Redux/05-Map Dispatch to Props.js
@@ -0,0 +1,15 @@
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// change code below this line
+const mapDispatchToProps = (dispatch) => {
+    return {
+        submitNewMessage: (message)=>{
+            dispatch(addMessage(message))
+        }
+    }
+}
