8c1556f82262af31d77f2354da2f9b01430db566
AndresJiAr <admin@AndresJiAr.dev>
2020-08-30 12:01:51 +0200

    Update app.js

--- JavaScript Calculator/app.js
+++ JavaScript Calculator/app.js
@@ -1,91 +1,91 @@
 
 $(document).ready(function(){
-    var inputs=[""];
-    var totalString;
-    var operators1=["+","-","/","*"];
-    var operators2=["."];
-    var nums=[0,1,2,3,4,5,6,7,8,9];
-  
-  
-    function getValue(input){
-      if(operators2.includes(inputs[inputs.length-1]===true && input===".")){
-  
-      }
-      else if(inputs.length===1 && operators1.includes(inputs)===false){
-        inputs.push(input);
-  
-      }
-  
-  
-      else if (operators1.includes(inputs[inputs.length-1])===false){
-        inputs.push(input);
-      }
-      else if (nums.includes(Number(input))){
-        inputs.push(input);
-      }
-  
-  
+  var inputs=[""];
+  var totalString;
+  var operators1=["add","subtract","divide","multiply"];
+  var operators2=["decimal"];
+  var nums=["zero","one","two","three","four","five","six","seven","eight","nine"];
+
+
+  function getValue(input){
+    if(operators2.includes(inputs[inputs.length-1]===true && input===".")){
+
+    }
+    else if(inputs.length===1 && operators1.includes(inputs)===false){
+      inputs.push(input);
+
+    }
+
+
+    else if (operators1.includes(inputs[inputs.length-1])===false){
+      inputs.push(input);
+    }
+    else if (nums.includes(Number(input))){
+      inputs.push(input);
+    }
+
+
+    update();
+  }
+  function update(){
+    totalString=inputs.join("");
+
+    if(totalString.length>=11){
+      $('#steps').css('font-size','45px');
+
+    }
+    if(totalString.length > 12){
+      $('#steps').css('font-size','53px');
+     
+      inputs=[""];
+      update();
+
+    }
+
+    $("#steps").html(totalString);
+
+  }
+  function getTotal(){
+    totalString=inputs.join("");
+    $("#steps").html(eval(totalString))
+  }
+  function square(i){
+    var a = Math.pow(i,2);
+    $("#steps").text(eval(a))
+   
+  }
+  function squrRoot(val){
+    var result=Math.sqrt(val);
+    
+    $('#steps').css('font-size','30px');
+    $("#steps").text(eval(result))
+  }
+  $("a").on("click",function(){
+    if(this.id==="clear"){
+      inputs=[""];
       update();
     }
-    function update(){
-      totalString=inputs.join("");
-  
-      if(totalString.length>=11){
-        $('#steps').css('font-size','45px');
-  
-      }
-      if(totalString.length > 12){
-        $('#steps').css('font-size','53px');
-        alert("Sorry no more input is allowed Ёпте :)");
-        inputs=[""];
-        update();
-  
-      }
-  
-      $("#steps").html(totalString);
-  
+    else if(this.id==="backOne"){
+      inputs.pop();
+      update();
     }
-    function getTotal(){
-      totalString=inputs.join("");
-      $("#steps").html(eval(totalString))
+    else if (this.id==="equals"){
+      getTotal();
+
     }
-    function square(i){
-      var a = Math.pow(i,2);
-      $("#steps").text(eval(a))
-     
-    }
-    function squrRoot(val){
-      var result=Math.sqrt(val);
-      
-      $('#steps').css('font-size','30px');
-      $("#steps").text(eval(result))
-    }
-    $("a").on("click",function(){
-      if(this.id==="deleteAll"){
-        inputs=[""];
-        update();
+   
+    else{
+      if(inputs[inputs.length-1].indexOf("add","subtract","divide","multiply","decimal")===-1){
+        getValue(this.id);
+
       }
-      else if(this.id==="backOne"){
-        inputs.pop();
-        update();
-      }
-      else if (this.id==="total"){
-        getTotal();
-  
-      }
-     
       else{
-        if(inputs[inputs.length-1].indexOf("+","-","/","*",".")===-1){
-          getValue(this.id);
-  
-        }
-        else{
-          getValue(this.id);
-        }
+        getValue(this.id);
       }
-    });
-  
+    }
   });
+
+});
   /*/$(document).ready(function(){
   var inputs=[""];
   var totalString;
