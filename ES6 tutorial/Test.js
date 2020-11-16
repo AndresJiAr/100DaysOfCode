9a9fd520224efddce16cbc775747914bd09052fb
AndresJiAr <info@andresjiar.dev>
2020-11-16 15:56:18 +0100

    Update Test.js

--- ES6 tutorial/Test.js
+++ ES6 tutorial/Test.js
@@ -5,3 +5,16 @@ let arr1 = [10,20]
 let arr2 = [30,40]
 let arr3 = [...arr1, ...arr2]
 console.log(arr3)
+
+function retStr(){
+    return "Hello World!!"
+
+}
+var val = retStr()
+console.log(val)
+
+function add(a,b){
+    var sum = a + b
+    console.log("The sum is: "+ sum)
+}
+add(1,2)
