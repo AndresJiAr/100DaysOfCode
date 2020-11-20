47bb1b3fd7ec6748a9347cd5afa72fba20158fec
AndresJiAr <info@andresjiar.dev>
2020-11-20 16:16:20 +0100

    Update 01-Diff Two Arrays.js

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/01-Diff Two Arrays.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/01-Diff Two Arrays.js
@@ -2,13 +2,13 @@ function diffArray(arr1, arr2) {
     var newArr = [];
   
     arr1.forEach(item => {
-      if(arr1.indexOf(item) === -1){
+      if(arr2.indexOf(item) === -1){
         newArr.push(item);
       }
     });
   
     arr2.forEach(item => {
-      if(arr2.indexOf(item) === -1){
+      if(arr1.indexOf(item) === -1){
         newArr.push(item);
     }
     });
