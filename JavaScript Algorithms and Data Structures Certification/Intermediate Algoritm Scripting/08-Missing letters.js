164861b91db105c7508c75215f2fc5e63ec523d1
AndresJiAr <info@andresjiar.dev>
2020-12-11 14:44:53 +0100

    Update 08-Missing letters.js
    Commit: Missing letters

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/08-Missing letters.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/08-Missing letters.js
@@ -1,18 +1,30 @@
 function fearNotLetter(str) {
-    var letters = "abcdefghijklmnopqrstuvwxyz";
-    letters = letters.split("");
-    str = str.split("");
-    
-      if(str === letters){
-        return undefined;
+  // Declare variable
+  var arrToCheck = [];
+  var notHere;
+
+  // Loop over the str and push the charCode number
+  for (var i = 0; i < str.length; i++) {
+    arrToCheck.push(str.charCodeAt(i));
+  }
+
+  // loop over the arrToCheck and check which number is missing
+  for (var x = 1; x < arrToCheck.length; x++) {
+    if (arrToCheck[x] - arrToCheck[x - 1] !== 1) {
+      // arrToCheck is the number + 1 so that why we -1
+      notHere = arrToCheck[x] - 1;
     }
-    for (var i = 0; i<str.length; i++){
-      if (str[i]!=letters[i]){
-        return letters[i];
-      }
-    }
-      return str;
-    }
-    
-    console.log(fearNotLetter("stvwx"));
-    
+  }
+
+  // Return undefined if no miss
+  if (!notHere) {
+    notHere = undefined;
+  } else {
+    // Return the string from the char code
+    notHere = String.fromCharCode(notHere);
+  }
+
+  return notHere;
+}
+
+fearNotLetter("abce");
