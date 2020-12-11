7fcd6db160eacdd8788858557f2d1c53b6fb28ff
AndresJiAr <info@andresjiar.dev>
2020-12-11 14:43:13 +0100

    Update 06-Search and Replace.js
    commit: Search and Replace

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/06-Search and Replace.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algoritm Scripting/06-Search and Replace.js
@@ -1,10 +1,22 @@
-function myReplace(str, before, after) {
-    if (before[0].toLowerCase() !== before[0]) {
-      after = after[0].toUpperCase() + after.slice(1);
+
+    function myReplace(str, before, after) {
+      var afterString = after;
+      if (before.charCodeAt(0) <= 90 && before.charCodeAt(0) >= 65) {
+        afterString = after.split("").map(function(letter, index) {
+          if (index === 0) {
+            return letter.toUpperCase();
+          }
+          return letter;
+        }).join("");
+      } else if (before.charCodeAt(0) <= 122 && before.charCodeAt(0) >= 97) {
+        afterString = after.split("").map(function(letter, index) {
+          if (index === 0) {
+            return letter.toLowerCase();
+          }
+          return letter;
+        }).join("");
+      }
+      var newStr = str.replace(new RegExp(before, 'g'), afterString);
+      return newStr;
     }
-    
-      return str.replace(before, after);
-    }
-    
-    console.log(myReplace("I think we should look up there", "up", "Down"));
-    
+    myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
